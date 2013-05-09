5ca8d1c75222c6aa1aba86f9a2c92d0ebf73f5be
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-05-08 23:14:16 -0400

    extracted looping through files into a method

--- src/alignments/Main.scala
+++ src/alignments/Main.scala
@@ -10,12 +10,6 @@ object Main {
 
   def main(args: Array[String]) {
 
-    /*
-    val out = new java.io.FileWriter("dev.out")
-    out.write("hey")
-    out.close()
-    System.exit(0)
-    */
     val start = Platform.currentTime
 
     val n = getN()
@@ -26,9 +20,9 @@ object Main {
 
     val startEm = Platform.currentTime
 
-    // TODO refactor code using method to concurrently loop through files
     // TODO move EM algorithm into separate method
 
+    // TODO find a prettier way to do this
     val temp = collection.mutable.Set[(String, String)]()
     t foreach {
       case (word1, map) =>
@@ -39,35 +33,28 @@ object Main {
     }
 
     println("also number of f|e combinations in t:" + temp.size)
-    //System.exit(0)
 
     val S = 5
     1 to S foreach { iter =>
       println("Starting iteration #" + iter)
 
-      val lang1Lines = Source.fromFile("corpus.en", "utf-8").getLines
-      val lang2Lines = Source.fromFile("corpus.es", "utf-8").getLines
-
       val c1 = collection.mutable.Map[String, Double]()
       val c2 = collection.mutable.Map[(String, String), Double]()
 
-      for (((line1, line2), index) <- lang1Lines zip lang2Lines zipWithIndex) {
-        if ((index+1) % 200 == 0) println("line#:" + (index + 1))
-        if (!line1.trim.isEmpty && !line2.trim.isEmpty) {
+      loopThroughFiles("corpus.en", "corpus.es")((line1: String, line2: String, index: Int) => {
 
-          line2 split " " foreach { word2 =>
+        line2 split " " foreach { word2 =>
 
-            val denom = ((line1 split " ") :+ NULL).foldLeft(0.0)((acc, word1) => acc + t(word1)(word2))
-            (line1 split " ") :+ NULL foreach { word1 =>
-              val delta = t(word1)(word2) / denom
-              c2((word1, word2)) = c2.getOrElse((word1, word2), 0.0) + delta
-              c1(word1) = c1.getOrElse(word1, 0.0) + delta
-            }
-
+          val denom = ((line1 split " ") :+ NULL).foldLeft(0.0)((acc, word1) => acc + t(word1)(word2))
+          (line1 split " ") :+ NULL foreach { word1 =>
+            val delta = t(word1)(word2) / denom
+            c2((word1, word2)) = c2.getOrElse((word1, word2), 0.0) + delta
+            c1(word1) = c1.getOrElse(word1, 0.0) + delta
           }
 
         }
-      }
+
+      })
 
       temp foreach {
         case (word1, word2) =>
@@ -80,51 +67,44 @@ object Main {
     println("number of e words in t:" + t.size)
     println("number of f|e combinations in t:" + t.foldLeft(0) { case (acc, (_, map)) => acc + map.size })
 
-    val end = Platform.currentTime
-
-    println("Total time=" + (end - start) / 1000.0 + "s")
-    println("EM time=" + (end - startEm) / 1000.0 + "s")
+    val endEm = Platform.currentTime
 
     // Writing dev alignments out
-    val lang1Lines = Source.fromFile("dev.en", "utf-8").getLines
-    val lang2Lines = Source.fromFile("dev.es", "utf-8").getLines
-
     val outputFile = new java.io.FileWriter("dev.out")
 
-    for (((line1, line2), index) <- lang1Lines zip lang2Lines zipWithIndex) {
-      if ((index+1) % 200 == 0) println("line#:" + (index + 1))
-      if (!line1.trim.isEmpty && !line2.trim.isEmpty) {
+    loopThroughFiles("dev.en", "dev.es")((line1: String, line2: String, index: Int) => {
 
-        for ((word2, index2) <- line2 split " " zipWithIndex) {
+      for ((word2, index2) <- line2 split " " zipWithIndex) {
 
-          val (_, maxIndex) = ((NULL +: (line1 split " ")) zipWithIndex).maxBy { case (word1, index1) => t(word1)(word2) }
-          outputFile.write((index + 1) + " " + maxIndex + " " + index2 + "\n")
-
-        }
+        val (_, maxIndex) = ((NULL +: (line1 split " ")) zipWithIndex).maxBy { case (word1, index1) => t(word1)(word2) }
+        outputFile.write((index + 1) + " " + maxIndex + " " + index2 + "\n")
 
       }
-    }
+
+    })
 
     outputFile.close()
+
+    val end = Platform.currentTime
+
+    println("EM time=" + (endEm - startEm) / 1000.0 + "s")
+    println("Total time=" + (end - start) / 1000.0 + "s")
 
   }
 
   def getN() = {
 
-    val lang1Lines = Source.fromFile("corpus.en", "utf-8").getLines
-    val lang2Lines = Source.fromFile("corpus.es", "utf-8").getLines
-
     val n = collection.mutable.Map[String, collection.mutable.Set[String]]()
 
-    for ((line1, line2) <- lang1Lines zip lang2Lines) {
-      if (!line1.trim.isEmpty && !line2.trim.isEmpty) {
-        (line1 split " ") :+ NULL foreach (word1 =>
-          if (n.contains(word1))
-            n(word1) ++= line2.split(" ")
-          else
-            n(word1) = collection.mutable.Set[String]() ++= line2.split(" "))
-      }
-    }
+    loopThroughFiles("corpus.en", "corpus.es")((line1: String, line2: String, index: Int) => {
+
+      (line1 split " ") :+ NULL foreach (word1 =>
+        if (n.contains(word1))
+          n(word1) ++= line2.split(" ")
+        else
+          n(word1) = collection.mutable.Set[String]() ++= line2.split(" "))
+
+    })
 
     n
   }
@@ -132,25 +112,34 @@ object Main {
   def initializeT(n: collection.mutable.Map[String, collection.mutable.Set[String]]) = {
 
     val translationParams = collection.mutable.Map[String, collection.mutable.Map[String, Double]]()
-
-    val lang1Lines = Source.fromFile("corpus.en", "utf-8").getLines
-    val lang2Lines = Source.fromFile("corpus.es", "utf-8").getLines
-
     val transParamEst = new TranslationParamEstimator
 
-    for (((line1, line2), index) <- lang1Lines zip lang2Lines zipWithIndex) {
-      if ((index+1) % 200 == 0) println("line#:" + (index + 1))
+    loopThroughFiles("corpus.en", "corpus.es")((line1: String, line2: String, index: Int) => {
+
+      (line1 split " ") :+ NULL foreach (word1 =>
+        line2 split " " foreach (word2 =>
+          if (translationParams.contains(word1))
+            translationParams(word1) += (word2 -> transParamEst.estimate(word2, word1, n))
+          else
+            translationParams(word1) = collection.mutable.Map[String, Double](word2 -> transParamEst.estimate(word2, word1, n))))
+
+    })
+
+    translationParams
+  }
+
+  def loopThroughFiles(file1: String, file2: String)(funcToPerform: (String, String, Int) => _) {
+
+    val file1Lines = Source.fromFile(file1, "utf-8").getLines
+    val file2Lines = Source.fromFile(file2, "utf-8").getLines
+
+    for (((line1, line2), index) <- file1Lines zip file2Lines zipWithIndex) {
+      if ((index + 1) % 200 == 0) println("line#:" + (index + 1))
       if (!line1.trim.isEmpty && !line2.trim.isEmpty) {
-        (line1 split " ") :+ NULL foreach (word1 =>
-          line2 split " " foreach (word2 =>
-            if (translationParams.contains(word1))
-              translationParams(word1) += (word2 -> transParamEst.estimate(word2, word1, n))
-            else
-              translationParams(word1) = collection.mutable.Map[String, Double](word2 -> transParamEst.estimate(word2, word1, n))))
+        funcToPerform(line1, line2, index)
       }
     }
 
-    translationParams
   }
 
 }
