8d0b96b702fe8ea631d67471ef289e299a883eec
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-06-11 23:00:46 -0400

    added code to find intersection points in the alignment matrix

--- src/main/scala/MachineTranslator.scala
+++ src/main/scala/MachineTranslator.scala
@@ -1,6 +1,7 @@
 package main.scala
 
 import com.typesafe.config.Config
+import Utils.loopThroughFiles
 
 class MachineTranslator {
 
@@ -21,6 +22,34 @@ class MachineTranslator {
     initializeIbmModel2(lang2FilePath, lang1FilePath, lang2ParamsReadFile, lang2ParamsWriteFile, ibm2Lang2)
 
     // TODO buildLexicon
+    loopThroughFiles(lang1FilePath, lang2FilePath)((line1: String, line2: String, index: Int) => {
+
+      //each position has the index of the lang1 word it aligns to (starting from 0 = NULL)
+      val lang2Alignments = ibm2Lang1.extractAlignments(line1, line2)
+
+      val lang1Alignments = ibm2Lang2.extractAlignments(line2, line1)
+
+      val lang2FinalAlignments = new collection.mutable.ArrayBuffer[Seq[Int]](lang2Alignments.size)
+      val lang1FinalAlignments = new collection.mutable.ArrayBuffer[Seq[Int]](lang1Alignments.size)
+
+      (lang2Alignments zipWithIndex) foreach {
+        case (lang1Index, lang2Index) =>
+          if (lang1Index > 0 && lang1Alignments(lang1Index - 1) == lang2Index + 1)
+            lang2FinalAlignments += IndexedSeq[Int](lang1Index - 1)
+          else lang2FinalAlignments += null
+      }
+
+      (lang1Alignments zipWithIndex) foreach {
+        case (lang2Index, lang1Index) =>
+          if (lang2Index > 0 && lang2Alignments(lang2Index - 1) == lang1Index + 1)
+            lang1FinalAlignments += IndexedSeq[Int](lang2Index - 1)
+          else lang1FinalAlignments += null
+      }
+
+      println(lang2FinalAlignments)
+      println(lang1FinalAlignments)
+
+    })
 
   }
 
