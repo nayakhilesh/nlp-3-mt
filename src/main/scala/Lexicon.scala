6ee57ff0b1f9651e8d12a53d52ba6845d0448017
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-06-21 22:15:26 -0400

    refactoring

--- src/main/scala/Lexicon.scala
+++ src/main/scala/Lexicon.scala
@@ -4,29 +4,18 @@ import scala.collection.mutable.ArrayBuffer
 
 class Lexicon {
 
-  val translatedPairs = new collection.mutable.HashSet[(Array[String], Array[String])]
-  val c2 = new collection.mutable.HashMap[(Array[String], Array[String]), Int]
-  val c1 = new collection.mutable.HashMap[Array[String], Int]
+  private[this] val translatedPairs = new collection.mutable.HashSet[(Array[String], Array[String])]
+  private[this] val c2 = new collection.mutable.HashMap[(Array[String], Array[String]), Int]
+  private[this] val c1 = new collection.mutable.HashMap[Array[String], Int]
 
   def add(lang1Alignments: Seq[Int], lang2Alignments: Seq[Int], line1: String, line2: String) {
 
     val lang2FinalAlignments = new ArrayBuffer[ArrayBuffer[Int]](lang2Alignments.size)
     val lang1FinalAlignments = new ArrayBuffer[ArrayBuffer[Int]](lang1Alignments.size)
 
-    // TODO extract into method
-    (lang2Alignments zipWithIndex) foreach {
-      case (lang1Index, lang2Index) =>
-        if (lang1Index > 0 && lang1Alignments(lang1Index - 1) == lang2Index + 1)
-          lang2FinalAlignments += (ArrayBuffer[Int]() += lang1Index - 1)
-        else lang2FinalAlignments += ArrayBuffer[Int]()
-    }
-
-    (lang1Alignments zipWithIndex) foreach {
-      case (lang2Index, lang1Index) =>
-        if (lang2Index > 0 && lang2Alignments(lang2Index - 1) == lang1Index + 1)
-          lang1FinalAlignments += (ArrayBuffer[Int]() += lang2Index - 1)
-        else lang1FinalAlignments += ArrayBuffer[Int]()
-    }
+    //find elements in intersection of alignments
+    findAlignmentIntersection(lang2Alignments, lang1Alignments, lang2FinalAlignments)
+    findAlignmentIntersection(lang1Alignments, lang2Alignments, lang1FinalAlignments)
 
     //order matters, though which is better?
     growAlignments(lang2FinalAlignments, lang1FinalAlignments, lang2Alignments, lang1Alignments)
@@ -48,26 +37,10 @@ class Lexicon {
           lengthLang1 = 0
           while (lengthLang1 < lang1FinalAlignments.size - startLang1) {
 
-            var foundException = false
-
-            // TODO extract into method
-            var temp2 = startLang2
-            while (!foundException && temp2 <= startLang2 + lengthLang2) {
-              val valueSeq = lang2FinalAlignments(temp2)
-              if (valueSeq.size == 0 || valueSeq.exists { value => value < startLang1 || value > (startLang1 + lengthLang1) })
-                foundException = true
-              temp2 += 1
-            }
-
-            var temp1 = startLang1
-            while (!foundException && temp1 <= startLang1 + lengthLang1) {
-              val valueSeq = lang1FinalAlignments(temp1)
-              if (valueSeq.size == 0 || valueSeq.exists { value => value < startLang2 || value > (startLang2 + lengthLang2) })
-                foundException = true
-              temp1 += 1
-            }
-
-            if (!foundException) {
+            if (checkConsistent(startLang2, lengthLang2,
+              startLang1, lengthLang1, lang2FinalAlignments) &&
+              checkConsistent(startLang1, lengthLang1,
+                startLang2, lengthLang2, lang1FinalAlignments)) {
               val phraseLang2 = words2.slice(startLang2, startLang2 + lengthLang2 + 1)
               val phraseLang1 = words1.slice(startLang1, startLang1 + lengthLang1 + 1)
               //println(arrayToString(phraseLang2) + "|" + arrayToString(phraseLang1))
@@ -89,31 +62,57 @@ class Lexicon {
 
   }
 
+  private[this] def checkConsistent(startLang2: Int, lengthLang2: Int,
+    startLang1: Int, lengthLang1: Int,
+    lang2FinalAlignments: ArrayBuffer[ArrayBuffer[Int]]): Boolean = {
+
+    var temp = startLang2
+    while (temp <= startLang2 + lengthLang2) {
+      val valueSeq = lang2FinalAlignments(temp)
+      if (valueSeq.size == 0 || valueSeq.exists { value => value < startLang1 || value > (startLang1 + lengthLang1) })
+        false
+      temp += 1
+    }
+    true
+
+  }
+
+  private[this] def findAlignmentIntersection(lang2Alignments: Seq[Int], lang1Alignments: Seq[Int],
+    lang2FinalAlignments: ArrayBuffer[ArrayBuffer[Int]]) {
+
+    (lang2Alignments zipWithIndex) foreach {
+      case (lang1Index, lang2Index) =>
+        if (lang1Index > 0 && lang1Alignments(lang1Index - 1) == lang2Index + 1)
+          lang2FinalAlignments += (ArrayBuffer[Int]() += lang1Index - 1)
+        else lang2FinalAlignments += ArrayBuffer[Int]()
+    }
+
+  }
+
   // TODO
-  def estimate(words1: Array[String], words2: Array[String]): Double = {
+  def estimate(wordsLang1: Array[String], wordsLang2: Array[String]): Double = {
     0.0
   }
 
-  // TODO descriptive variable names
   private[this] def growAlignments(lang2FinalAlignments: IndexedSeq[ArrayBuffer[Int]],
     lang1FinalAlignments: IndexedSeq[ArrayBuffer[Int]],
     lang2Alignments: Seq[Int], lang1Alignments: Seq[Int]) {
 
     (lang2FinalAlignments zipWithIndex) foreach {
-      case (value1Seq, index2) =>
-        if (value1Seq.size == 0) {
+      case (lang1Seq, index2) =>
+        if (lang1Seq.size == 0) {
 
           var count = -1
-          val alignment1 = lang2Alignments(index2) - 1
-          if (alignment1 >= 0) {
-            count = countNeighbours(alignment1, index2, lang2FinalAlignments, lang1FinalAlignments)
+          val alignedToLang1Index = lang2Alignments(index2) - 1
+          if (alignedToLang1Index >= 0) {
+            count = countNeighbours(alignedToLang1Index, index2, lang2FinalAlignments, lang1FinalAlignments)
           }
 
           var count1 = -1
           var row1 = 0
           (lang1Alignments zipWithIndex) foreach {
-            case (value2, index1) =>
-              if (value2 - 1 == index2) {
+            case (lang2Index, index1) =>
+              if (lang2Index - 1 == index2) {
                 val tempCount = countNeighbours(index1, index2, lang2FinalAlignments, lang1FinalAlignments)
                 if (tempCount > count1) {
                   count1 = tempCount
@@ -124,8 +123,8 @@ class Lexicon {
 
           if (!(count == -1 && count1 == -1)) {
             if (count >= count1) {
-              lang2FinalAlignments(index2) += alignment1
-              lang1FinalAlignments(alignment1) += index2
+              lang2FinalAlignments(index2) += alignedToLang1Index
+              lang1FinalAlignments(alignedToLang1Index) += index2
             } else {
               lang2FinalAlignments(index2) += row1
               lang1FinalAlignments(row1) += index2
